322405d93853fe6020d27af26078cd7cfca22795
mustan-ali <mustan9900@gmail.com>
2021-11-18 23:04:14 +0500

    Added Typecasting and Data type updated

--- arithmetic.c
+++ arithmetic.c
@@ -2,29 +2,26 @@
 
 int main()
 {
-    /*int a=5,b=2; 
+  /*int a=5,b=2; 
       
     printf("a+b= %d\n", a + b); 
     printf("a-b= %d\n", a - b);
     printf("a*b= %d\n", a * b);
     printf("a/b= %d\n", a / b);
-    printf("a+b= %d\n", a + b);
     */
 
-    int a;
-    float b;
+  int a, b;
 
-    printf("Enter num a\n");
-    scanf("%d", &a);
+  printf("Enter num a\n");
+  scanf("%d", &a);
 
-    printf("Enter num b\n");
-    scanf("%f", &b);
+  printf("Enter num b\n");
+  scanf("%d", &b);
 
-    printf("a+b= %f\n", a + b);
-    printf("a-b= %f\n", a - b);
-    printf("a*b= %f\n", a * b);
-    printf("a/b= %f\n", a / b);
-    printf("a+b= %f\n", a + b);
+  printf("a+b= %d\n", a + b);
+  printf("a-b= %d\n", a - b);
+  printf("a*b= %f\n", (float)a * (float)b);
+  printf("a/b= %f\n", (float)a / (float)b);
 
   return 0;
 }
