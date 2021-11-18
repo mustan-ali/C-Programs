688fcf21d20554d57c861718897c0c8c2209554d
mustan-ali <mustan9900@gmail.com>
2021-11-18 22:58:57 +0500

    added loop for multiplication

--- table.c
+++ table.c
@@ -2,15 +2,21 @@
 
 int main()
 {
-int num;
+int num,i;
 printf("Enter number ");
 scanf("%d",&num);
-
+/*
 printf("%d * 1= %d\n",num, num * 1);
 printf("%d * 2= %d\n",num, num * 2);
 printf("%d * 3= %d\n",num, num * 3);
 printf("%d * 4= %d\n",num, num * 4);
 printf("%d * 5= %d\n",num, num * 5);
+*/
+for (i=0; i <= 5; i++)
+{
+    printf("%d * %d = %d\n",num,i,num*i);
+}
+
 
 return 0;
 }
