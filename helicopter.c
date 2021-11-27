7a2db949a47ad5610f74bff58c85517e3275a130
mustan-ali <mustan9900@gmail.com>
2021-11-27 23:22:24 +0500

    Create helicopter.c

---
+++ helicopter.c
@@ -0,0 +1,31 @@
+int main()
+{
+label1:
+    system("cls");
+    printf("  ROFL-ROFL-LOL\n");
+    printf("              A\n");
+    printf("         /---------\n");
+    printf(" LOL=====        [] \\\n");
+    printf("  L       \\          \\\n");
+    printf("           \\          \\\n");
+    printf("            \\__________]\n");
+    printf("               I    I\n");
+    printf("             ----------\n");
+
+    goto label2;
+
+label2:
+    system("cls");
+    printf("              LOL-ROFL-ROFL\n");
+    printf("              A\n");
+    printf("  L      /---------\n");
+    printf(" LOL=====        [] \\\n");
+    printf("          \\          \\\n");
+    printf("           \\          \\\n");
+    printf("            \\__________]\n");
+    printf("               I    I\n");
+    printf("             ----------\n");
+
+    goto label1;
+    return 0;
+}
